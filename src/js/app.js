b7607ce927dbab037a024e2811bb13dc0482fb8d
Jordan <jordanasenovzhelyazkov@gmail.com>
2021-09-26 19:19:25 +0300

    fixed

--- src/js/app.js
+++ src/js/app.js
@@ -2,9 +2,14 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-
+  const discountEl = document.querySelector('.discounts');
+  if(screen.width > 599){
+    discountEl.style.visibility = 'hidden';
+  }
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
     alert("💣");
   });
 });
+
+
