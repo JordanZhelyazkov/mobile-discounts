0c45c5c7fe775a75146a3c6112b049f9c09b1ba8
Jordan <jordanasenovzhelyazkov@gmail.com>
2021-09-26 20:09:39 +0300

    media query

--- src/js/app.js
+++ src/js/app.js
@@ -2,18 +2,19 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-  const discountEl = document.querySelector('.discounts');
-  console.log(discountEl);
-  if(screen.width < 600){
-    discountEl.style.visibility = 'visible';
-  } else {
-    discountEl.style.visibility = 'hidden';
-  }
-  console.log(screen.width);
+ 
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
     alert("💣");
   });
 });
 
+//  const discountEl = document.querySelector('.discounts');
+//   console.log(discountEl);
+//   if(screen.width < 600){
+//     discountEl.style.visibility = 'visible';
+//   } else {
+//     discountEl.style.visibility = 'hidden';
+//   }
+//   console.log(screen.width);
 
