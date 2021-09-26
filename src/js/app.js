dc605689cab2d5b2dbb34d2d3df71b7489d7a380
Jordan <jordanasenovzhelyazkov@gmail.com>
2021-09-26 19:43:23 +0300

    finally fixed with js

--- src/js/app.js
+++ src/js/app.js
@@ -3,9 +3,11 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
   const discountEl = document.querySelector('.discounts');
+  console.log(discountEl);
   if(screen.width > 599){
-    discountEl.style.visibility = 'hidden';
+    discountEl.style.display = 'none';
   }
+  console.log(screen.width);
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
     alert("💣");
